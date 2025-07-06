76d7624f70646f424945f84a006058f00048e2d6
AslamJM <jma8744@gmail.com>
2025-07-06 10:58:22 +0530

    fix err

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -1,24 +1,10 @@
 import PageTitle from "@/components/page-title";
 import { getProjectBySlug } from "@/sanity/projects";
 import { SanityImageAssetDocument } from "next-sanity";
-import { PageProps } from "../../../../.next/types/app/projects/[slug]/page";
 
-export default async function SingleProjectPage(props: PageProps) {
-  const id = await props.params;
-  const project = await getProjectBySlug(id.slug);
+export default async function SingleProjectPage() {
+  // const id = await props.params;
+  // const project = await getProjectBySlug(id.slug);
 
-  return (
-    <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-      <div className="col-span-1 space-y-2">
-        <PageTitle title={project.title} />
-        <p className="font-semibold">{project.client}</p>
-        <p>{project.description}</p>
-      </div>
-      <div className=" col-span-2 grid grid-cols-3 gap-4">
-        {project.media.map((m: SanityImageAssetDocument, i: number) => (
-          <img src={m.asset.url} key={`image=${i}`} className="rounded" />
-        ))}
-      </div>
-    </div>
-  );
+  return <div className="grid grid-cols-1 md:grid-cols-3 gap-4"></div>;
 }
